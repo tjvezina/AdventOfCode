728ef3c9331cc43e7c1937863719f8fa19142dea
Tyler Vezina <tvezina@reli.sh>
2019-12-05 14:47:38 -0500

    2019 5A/5B complete

---
+++ 2019/AOC-5B/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+public static class Program {
+    private static void Main(string[] args) {
+        IntCode intCode = new IntCode();
+        intCode.Load(File.ReadAllLines("input.txt")[0]);
+        intCode.Execute();
+    }
+}
