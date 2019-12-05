d1f32a40beb4b99c7b811c16455c22996c169851
Tyler Vezina <tvezina@reli.sh>
2019-12-05 16:32:47 -0500

    2015 8A/8B complete

---
+++ 2015/AOC-8B/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+public static class Program {
+    private static void Main(string[] args) {
+        string[] input = File.ReadAllLines("input.txt");
+
+        Console.WriteLine(input.Sum(GetEscapedCount) - input.Sum(s => s.Length));
+    }
+
+    private static int GetEscapedCount(string str) {
+        for (int i = str.Length - 1; i >= 0; --i) {
+            if (str[i] == '\"' || str[i] == '\\') {
+                str = str.Substring(0, i) + "\\" + str.Substring(i);
+            }
+        }
+
+        str = $"\"{str}\"";
+
+        return str.Length;
+    }
+}
