6eab0df1b134321dada54d179be232b1ab504ed3
Tyler Vezina <tvezina@reli.sh>
2019-12-06 16:38:51 -0500

    2015 10B started

---
+++ 2015/AOC-10B/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Program {
+    private const string INPUT = "1113122113";
+    private const int ITERATIONS = 50;
+
+    private static void Main(string[] args) {
+        MatrixInt input = new MatrixInt(new int[,] { {1}, {1}, {1} });
+
+        MatrixInt dna = new MatrixInt(new int[,] {
+            { 1, 1, 0 },
+            { 0, 0, 2 },
+            { 0, 1, 0 }
+        });
+        
+        Console.WriteLine(MatrixInt.Power(dna, 4) * input);
+    }
+}
