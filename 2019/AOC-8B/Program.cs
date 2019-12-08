7dbdee67b641df948c73b0967272e39911f19a13
Tyler Vezina <tvezina@karmaninteractive.com>
2019-12-08 09:36:08 -0500

    2019 8A/8B complete

---
+++ 2019/AOC-8B/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public static class Program {
+    private static void Main(string[] args) {
+        SpaceImage image = new SpaceImage(25, 6, File.ReadAllLines("input.txt")[0]);
+        image.Draw();
+    }
+}
