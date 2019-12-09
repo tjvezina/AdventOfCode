a35af867dae01c19bd4fa017eda699ee3c25eb1b
Tyler Vezina <tvezina@karmaninteractive.com>
2019-12-09 08:34:17 -0500

    2019 9 complete

---
+++ 2019/AOC-9A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+public static class Program {
+    private static void Main(string[] args) {
+        IntCode intCode = new IntCode(File.ReadAllLines("input.txt")[0]);
+        intCode.OnOutput += (o => Console.WriteLine("Output: " + o));
+        intCode.Begin();
+
+        while (intCode.state == IntCode.State.Waiting) {
+            Console.Write("Input : ");
+            intCode.Input(long.Parse(Console.ReadLine()));
+        }
+    }
+}
