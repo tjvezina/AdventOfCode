218d6b315d3eed0407511cc05614d8146e93cf5b
Tyler Vezina <tvezina@karmaninteractive.com>
2019-12-09 08:06:12 -0500

    2015 15A complete

---
+++ 2015/AOC-15A/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class Program {
+    private static Recipe _recipe;
+
+    private static void Main(string[] args) {
+        _recipe = new Recipe();
+        _recipe.MaximizeScore();
+    }
+}
