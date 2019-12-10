840c6128fb90da3bba34de5ee1e103f4bc6bec92
Tyler Vezina <tvezina@karmaninteractive.com>
2019-12-09 21:51:58 -0500

    2015 19 complete

---
+++ 2015/AOC-19A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public static class Program {
+    private static string _molecule;
+    private static List<KeyValuePair<string, string>> _replacements = new List<KeyValuePair<string, string>>();
+
+    private static void Main(string[] args) {
+        LoadInput();
+        CountSubMolecules();
+    }
+
+    private static void LoadInput() {
+        Queue<string> input = new Queue<string>(File.ReadAllLines("input.txt"));
+
+        string data;
+        while (!string.IsNullOrEmpty(data = input.Dequeue())) {
+            string[] parts = data.Split(' ');
+            _replacements.Add(new KeyValuePair<string, string>(parts[0], parts[2]));
+        }
+
+        _molecule = input.Dequeue();
+    }
+
+    private static void CountSubMolecules() {
+        HashSet<string> subMolecules = new HashSet<string>();
+
+        foreach ((string pattern, string replace) in _replacements) {
+            int i = 0;
+            while ((i = _molecule.IndexOf(pattern, i)) != -1) {
+                subMolecules.Add(_molecule.Remove(i, pattern.Length).Insert(i, replace));
+                ++i;
+            }
+        }
+
+        Console.WriteLine("Distinct submolecules: " + subMolecules.Count);
+    }
+}
---
+++ 2015/AOC-19B/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public static class Program {
+    private static string _molecule;
+    private static List<KeyValuePair<string, string>> _replacements = new List<KeyValuePair<string, string>>();
+
+    private static void Main(string[] args) {
+        LoadInput();
+        CountStepToMolecule();
+    }
+
+    private static void LoadInput() {
+        Queue<string> input = new Queue<string>(File.ReadAllLines("input.txt"));
+
+        string data;
+        while (!string.IsNullOrEmpty(data = input.Dequeue())) {
+            string[] parts = data.Split(' ');
+            _replacements.Add(new KeyValuePair<string, string>(parts[0], parts[2]));
+        }
+
+        _molecule = input.Dequeue();
+    }
+
+    private static void CountStepToMolecule() {
+        int Count(string match) {
+            int count = 0;
+            for (int i = 0; (i = _molecule.IndexOf(match, i)) != -1; ++i, ++count);
+            return count;
+        }
+
+        int steps = _molecule.Count(char.IsUpper) - Count("Rn") - Count("Ar") - 2 * Count("Y") - 1;
+
+        Console.WriteLine("Steps to molecule: " + steps);
+    }
+}
