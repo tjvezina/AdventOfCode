62f7e7ab3e785130239adea3ff0867a1c4cf2536
Tyler Vezina <tvezina@reli.sh>
2019-12-18 16:52:31 -0500

    Fixed/improved Point/Direction rotation functions

--- Utility/CoordSystem.cs
+++ Utility/CoordSystem.cs
@@ -43,17 +43,10 @@ namespace AdventOfCode {
             throw new Exception($"Failed to convert {p} to Direction, must be unit vector.");
         }
 
-        public static Point RotateCW(this Point p) => (system == CoordSystem.YUp ? new Point(p.y, -p.x) : new Point(-p.y, p.x));
-        public static Point RotateCCW(this Point p) => (system == CoordSystem.YUp ? new Point(-p.y, p.x) : new Point(p.y, -p.x));
+        public static Point Rotate90(this Point p) => new Point(-p.y, p.x);
+        public static Point Rotate270(this Point p) => new Point(p.y, -p.x);
 
-        public static Direction Rotate90(this Direction dir) {
-            switch (dir) {
-                case Direction.Up:    return Direction.Right;
-                case Direction.Right: return Direction.Down;
-                case Direction.Down:  return Direction.Left;
-                case Direction.Left:  return Direction.Up;
-            }
-            throw new Exception($"Unknown direction {dir}");
-        }
+        public static Point RotateCW(this Point p) => (system == CoordSystem.YUp ? Rotate270(p) : Rotate90(p));
+        public static Point RotateCCW(this Point p) => (system == CoordSystem.YUp ? Rotate90(p) : Rotate270(p));
     }
 }
--- Utility/Direction.cs
+++ Utility/Direction.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace AdventOfCode {
     public enum Direction {
         Right,
@@ -11,5 +14,40 @@ namespace AdventOfCode {
         public static bool IsPositive(this Direction dir) => dir == Direction.Right || dir == Direction.Up;
 
         public static int Sign(this Direction dir) => (dir.IsPositive() ? 1 : -1);
+
+        public static IEnumerable<Direction> GetOrthogonal(this Direction dir) {
+            yield return dir.RotateCW();
+            yield return dir.RotateCCW();
+        }
+
+        public static Direction GetOpposite(this Direction dir) {
+            switch (dir) {
+                case Direction.Right: return Direction.Left;
+                case Direction.Left:  return Direction.Right;
+                case Direction.Up:    return Direction.Down;
+                case Direction.Down:  return Direction.Up;
+            }
+            throw new Exception($"Unknown direction {dir}");
+        }
+
+        public static Direction RotateCW(this Direction dir) {
+            switch (dir) {
+                case Direction.Up:    return Direction.Right;
+                case Direction.Right: return Direction.Down;
+                case Direction.Down:  return Direction.Left;
+                case Direction.Left:  return Direction.Up;
+            }
+            throw new Exception($"Unknown direction {dir}");
+        }
+
+        public static Direction RotateCCW(this Direction dir) {
+            switch (dir) {
+                case Direction.Up:    return Direction.Left;
+                case Direction.Left:  return Direction.Down;
+                case Direction.Down:  return Direction.Right;
+                case Direction.Right: return Direction.Up;
+            }
+            throw new Exception($"Unknown direction {dir}");
+        }
     }
 }
