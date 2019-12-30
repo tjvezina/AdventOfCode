f0e21f9096d615f5095d90d1fd3df97a0738c557
Tyler Vezina <tvezina@karmaninteractive.com>
2019-12-29 20:38:22 -0500

    2015 4B optimized 4x faster

--- 2015/04/Challenge.cs
+++ 2015/04/Challenge.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 
 namespace AdventOfCode.Year2015.Day04 {
      public class Challenge : BaseChallenge {
         private const string KEY = "ckczppom";
+        private const int ThreadCount = 2;
+
+        private object _lockObj = new object();
+        private int _targetInput;
 
         public override string part1Answer => "117946";
-        public override (string, object) SolvePart1() => ("Desired input: ", FindInput("00000"));
+        public override (string, object) SolvePart1() {
+            int input = FindInput((hash) => hash[0] + hash[1] == 0 && hash[2] < 16); // Starts with 5 zeros
+            return ("Desired input: ", input);
+        }
         
         public override string part2Answer => "3938038";
-        public override (string, object) SolvePart2() => ("Desired input: ", FindInput("000000"));
+        public override (string, object) SolvePart2() {
+            // Unable to optimize further; calculating MD5 hash takes about 8 ticks, and there is no other way to find
+            // the answer than brute forcing almost 4 million hashes requiring about 32 million ticks (3.2 sec).
+            // 2 threads are faster, but more is slower (too much overhead?)
+            int input = FindInputThreaded((hash) => hash[0] + hash[1] + hash[2] == 0); // Starts with 6 zeros
+            return ("Desired input: ", input);
+        }
 
-        private int FindInput(string target) {
+        private int FindInput(Func<byte[], bool> isMatch) {
             for (int i = 1; i < int.MaxValue; ++i) {
                 string input = KEY + i;
-                if (CalculateMD5Hash(input).StartsWith(target)) {
+                if (isMatch(CalculateMD5Hash(input))) {
                     return i;
                 }
             }
@@ -25,11 +39,34 @@ namespace AdventOfCode.Year2015.Day04 {
             throw new Exception("Failed to find desired input");
         }
 
-        private string CalculateMD5Hash(string input) {
-            MD5 md5 = MD5.Create();
-            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
-            byte[] hash = md5.ComputeHash(inputBytes);
-            return hash.Select(b => $"{b:X2}").Aggregate((a, b) => a + b);
+        private int FindInputThreaded(Func<byte[], bool> isMatch) {
+            Thread[] threads = new Thread[ThreadCount];
+
+            for (int i = 0; i < ThreadCount; ++i) {
+                int start = i + 1;
+                threads[i] = new Thread(() => ThreadFindInput(isMatch, start));
+                threads[i].Start();
+            }
+
+            while (threads.Any(t => t.IsAlive));
+
+            return _targetInput;
         }
+
+        private void ThreadFindInput(Func<byte[], bool> isMatch, int start) {
+            for (int i = start; i < int.MaxValue; i += ThreadCount) {
+                string input = KEY + i;
+                if (isMatch(CalculateMD5Hash(input))) {
+                    lock (_lockObj) {
+                        _targetInput = i;
+                    }
+                    return;
+                }
+
+                if (_targetInput > 0 && i > _targetInput) break;
+            }
+        }
+
+        private byte[] CalculateMD5Hash(string input) => MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(input));
     }
 }
