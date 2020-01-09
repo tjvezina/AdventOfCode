e609ff860cd94bd145c2001b606dd57eeb6ffe35
Tyler Vezina <tvezina@karmaninteractive.com>
2020-01-09 08:28:42 -0500

    2015 24B complete

--- 2015/24/Challenge.cs
+++ 2015/24/Challenge.cs
@@ -5,72 +5,94 @@ using System.Linq;
 
 namespace AdventOfCode.Year2015.Day24 {
     public class Challenge : BaseChallenge {
-        private long[] _packages;
-        private long _groupSize;
+        private int[] _packages;
 
         public override void InitPart1() {
-            _packages = inputSet.Select(long.Parse).ToArray();
-            _groupSize = _packages.Sum() / 3;
+            _packages = inputSet.Select(int.Parse).ToArray();
         }
 
         public override string part1Answer => "10723906903";
         public override (string, object) SolvePart1() {
-            (long[] groupA, long[] groupB, long[] groupC) = FindBestGroups();
-
-            Console.WriteLine(groupA.Select(p => $"{p}").Aggregate((a, b) => $"{a}, {b}"));
-            Console.WriteLine(groupB.Select(p => $"{p}").Aggregate((a, b) => $"{a}, {b}"));
-            Console.WriteLine(groupC.Select(p => $"{p}").Aggregate((a, b) => $"{a}, {b}"));
-
-            return ("Quantum entanglement of Group A: ", groupA.Aggregate((a, b) => a * b));
+            List<int[]> groups = FindBestGroups(groupCount:3);
+            return ("Quantum entanglement of first group: ", GetQuantumEntanglement(groups[0]));
         }
         
-        public override string part2Answer => null;
+        public override string part2Answer => "74850409";
         public override (string, object) SolvePart2() {
-            return (null, null);
+            // Try some other counts, just for fun
+            FindBestGroups(groupCount:6);
+            //FindBestGroups(groupCount:8); // 8 is possible, but slow
+
+            List<int[]> groups = FindBestGroups(groupCount:4);
+            return ("Quantum entanglement of first group: ", GetQuantumEntanglement(groups[0]));
         }
 
-        private (long[] a, long[] b, long[] c) FindBestGroups() {
-            int groupCount = 1;
+        private long GetQuantumEntanglement(int[] group) => group.Select(p => (long)p).Aggregate((a, b) => a * b);
 
-            while (groupCount <= _packages.Length) {
-                IEnumerable<long[]> groups = DataUtil.GetAllCombinations(_packages, groupCount)
-                    .Where(g => g.Sum() == _groupSize);
+        private List<int[]> FindBestGroups(int groupCount) {
+            int packagesSum = _packages.Sum();
+            Debug.Assert(packagesSum % groupCount == 0, $"Unable to evenly divide packages into {groupCount} groups");
+            int groupSum = packagesSum / groupCount;
 
-                if (groups.Count() > 0) {
-                    long[] groupA = groups.OrderBy(g => g.Aggregate((a, b) => a * b)).First();
-                    FindSubGroups(groupA, out long[] groupB, out long[] groupC);
-                    return (groupA, groupB, groupC);
+            List<int[]> groups = new List<int[]>();
+
+            int groupSize = 1;
+            while (groupSize <= _packages.Length) {
+                IEnumerable<int[]> validGroups = DataUtil.GetAllCombinations(_packages, groupSize)
+                    .Where(g => g.Sum() == groupSum);
+
+                if (validGroups.Count() > 0) {
+                    groups.Add(validGroups.OrderBy(GetQuantumEntanglement).First());
+                    break;
                 }
-                
-                ++groupCount;
+
+                groupSize++;
             }
 
-            throw new Exception($"Failed to find a group of packages that satisfy challenge conditions");
+            Debug.Assert(groups.Count > 0, $"Failed to find any groups that total {groupSum}");
+
+            bool FindNextGroup(IEnumerable<int> except, int index = 1) {
+                if (index == groupCount - 1) {
+                    int[] lastGroup = _packages.Except(except).ToArray();
+                    groups.Insert(1, lastGroup);
+                    return true;
+                }
+
+                foreach (int[] subGroup in FindGroups(groupSum, except)) {
+                    IEnumerable<int> nextExcept = except.Concat(subGroup);
+                    if (FindNextGroup(nextExcept, index + 1)) {
+                        groups.Insert(1, subGroup);
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            FindNextGroup(groups[0]);
+
+            Debug.Assert(groups.Count == groupCount, "Failed to find secondary groups to match the first");
+
+            Console.WriteLine($" -- {groupCount} Groups -- ");
+            for (int i = 0; i < groups.Count; ++i) {
+                Console.WriteLine($"Group {i+1}: {groups[i].OrderBy(p => p).Select(p => $"{p}").Aggregate((a, b) => $"{a} {b}")}");
+            }
+
+            return groups;
         }
 
-        private void FindSubGroups(long[] groupA, out long[] groupB, out long[] groupC) {
-            long[] packages = _packages.Except(groupA).ToArray();
+        private IEnumerable<int[]> FindGroups(int groupSum, IEnumerable<int> except) {
+            int[] packages = _packages.Except(except).ToArray();
 
-            Stack<int> indices = new Stack<int>();
-            indices.Push(0);
+            Stack<int> indices = new Stack<int>(new[] { 0 });
 
             int iLast = packages.Length - 1;
             while (true) {
-                groupB = indices.Select(i => packages[i]).ToArray();
+                int[] group = indices.Select(i => packages[i]).ToArray();
 
-                int compare = groupB.Sum().CompareTo(_groupSize);
+                int compare = group.Sum().CompareTo(groupSum);
 
-                if (compare == 0) {
-                    groupC = new long[packages.Length - indices.Count];
-                    int iC = 0;
-                    for (int i = 0; i < packages.Length; i++) {
-                        if (!indices.Contains(i)) {
-                            groupC[iC++] = packages[i];
-                        }
-                    }
-
-                    if (groupC.Sum() == _groupSize) return;
-                }
+                if (compare == 0) yield return group;
 
                 if (compare >= 0 || indices.Peek() == iLast) {
                     // Base case: a single container matches the group size, or we're at the last index
@@ -83,8 +105,6 @@ namespace AdventOfCode.Year2015.Day24 {
                     indices.Push(indices.Peek() + 1);
                 }
             }
-
-            throw new Exception("Failed to find sub groups from the given set");
         }
     }
 }
