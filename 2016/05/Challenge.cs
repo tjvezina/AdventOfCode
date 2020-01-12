4ac073772c1a9e9048f7d6a5d5d9b5f81af1baba
Tyler Vezina <tvezina@karmaninteractive.com>
2020-01-12 00:16:07 -0500

    2016 5 optimized 3x faster, still slow

--- 2016/05/Challenge.cs
+++ 2016/05/Challenge.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -8,56 +7,59 @@ using System.Text;
 namespace AdventOfCode.Year2016.Day05 {
     public class Challenge : BaseChallenge {
         private const string DoorID = "wtnhxymk";
+        private const int PasswordLength = 8;
+        private const char Placeholder = '-';
+
+        private MD5 _md5 = MD5.Create();
 
         public override string part1Answer => "2414BC77";
-        public override (string, object) SolvePart1() => ("Password: ", GetPasswordA());
-        
-        public override string part2Answer => "437E60FC";
-        public override (string, object) SolvePart2() => ("Password: ", GetPasswordB());
-
-        private string GetPasswordA() {
+        public override (string, object) SolvePart1() {
             string password = string.Empty;
 
-            int input = 0;
-            while (true) {
-                byte[] hash = CalculateMD5Hash($"{DoorID}{input}");
+            foreach ((int input, byte[] hash) in GetValidHashes()) {
+                password += $"{hash[2]:X2}"[1];
+                Console.WriteLine($"Password: {password,-PasswordLength} ({input,10:N0})");
 
-                if (hash[0] == 0 && hash[1] == 0 && hash[2] < 16) { // Starts with 5 zeros
-                    password += $"{hash[2]:X2}"[1];
-                    Console.WriteLine($"Character found in input {input:N0}: {password}");
-
-                    if (password.Length == 8) break;
-                }
-
-                input++;
+                if (password.Length == PasswordLength) break;
             }
 
-            return password;
+            return ("Password: ", password);
         }
+        
+        public override string part2Answer => "437E60FC";
+        public override (string, object) SolvePart2() {
+            char[] password = new string(Placeholder, PasswordLength).ToCharArray();
 
-        private string GetPasswordB() {
-            char[] password = new string('_', 8).ToCharArray();
+            foreach ((int input, byte[] hash) in GetValidHashes()) {
+                byte index = hash[2];
+                if (index < 0x08 && password[index] == Placeholder) {
+                    password[index] = $"{hash[3]:X2}"[0];
+                    Console.WriteLine($"Password: {new string(password)} ({input,10:N0})");
 
-            int input = 0;
-            while (true) {
-                byte[] hash = CalculateMD5Hash($"{DoorID}{input}");
-
-                if (hash[0] == 0 && hash[1] == 0 && hash[2] < 16) { // Starts with 5 zeros
-                    byte index = hash[2];
-                    if (index < 8 && password[index] == '_') {
-                        password[index] = $"{hash[3]:X2}"[0];
-                        Console.WriteLine($"Character found in input {input:N0}: {new string(password)}");
-
-                        if (!password.Contains('_')) break;
-                    }
+                    if (!password.Contains(Placeholder)) break;
                 }
 
-                input++;
             }
 
-            return new string(password);
+            return ("Password: ", new string(password));
         }
 
-        private byte[] CalculateMD5Hash(string input) => MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(input));
+        private IEnumerable<(int input, byte[] hash)> GetValidHashes() {
+            byte[] ToBytes(int i) => Encoding.UTF8.GetBytes($"{DoorID}{i}");
+
+            byte[] buffer = ToBytes(0);
+
+            for (int i = 0; i < int.MaxValue; ++i) {
+                byte[] hash = _md5.ComputeHash(buffer);
+
+                if (hash[0] == 0 && hash[1] == 0 && hash[2] < 0x0F) { // Starts with 5 zeros
+                    yield return (i, hash);
+                }
+
+                if (buffer[buffer.Length - 1]++ == '9') {
+                    buffer = ToBytes(i);
+                }
+            }
+        }
     }
 }
