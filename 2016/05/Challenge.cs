5b81d5fffeee34853e8743e214a878124ec83631
Tyler Vezina <tvezina@karmaninteractive.com>
2020-01-16 15:45:59 -0500

    2016 5 optimized

--- 2016/05/Challenge.cs
+++ 2016/05/Challenge.cs
@@ -3,63 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 
 namespace AdventOfCode.Year2016.Day05 {
     public class Challenge : BaseChallenge {
         private const string DoorID = "wtnhxymk";
         private const int PasswordLength = 8;
-        private const char Placeholder = '-';
 
-        private MD5 _md5 = MD5.Create();
+        private const int ThreadCount = 12;
+
+        private SortedList<int, byte[]> _validHashes = new SortedList<int, byte[]>();
+        private int _maxInput;
 
         public override string part1Answer => "2414BC77";
         public override (string, object) SolvePart1() {
-            string password = string.Empty;
+            FindHashes(condition:() => _validHashes.Count >= PasswordLength);
 
-            foreach ((int input, byte[] hash) in GetValidHashes()) {
-                password += $"{hash[2]:X2}"[1];
-                Console.WriteLine($"Password: {password,-PasswordLength} ({input,10:N0})");
-
-                if (password.Length == PasswordLength) break;
-            }
+            string password = string.Concat(_validHashes.Take(PasswordLength).Select(d => $"{d.Value[2]:X2}"[1]));
 
             return ("Password: ", password);
         }
         
         public override string part2Answer => "437E60FC";
         public override (string, object) SolvePart2() {
-            char[] password = new string(Placeholder, PasswordLength).ToCharArray();
+            IEnumerable<byte> indices = Enumerable.Range(0, PasswordLength).Select(i => (byte)i);
 
-            foreach ((int input, byte[] hash) in GetValidHashes()) {
-                byte index = hash[2];
-                if (index < 0x08 && password[index] == Placeholder) {
-                    password[index] = $"{hash[3]:X2}"[0];
-                    Console.WriteLine($"Password: {new string(password)} ({input,10:N0})");
+            FindHashes(condition:() => !indices.Except(_validHashes.Select(d => d.Value[2])).Any());
 
-                    if (!password.Contains(Placeholder)) break;
-                }
+            IEnumerable<byte[]> passwordHashes = indices.Select(i => _validHashes.First(p => p.Value[2] == i).Value);
+            string password = string.Concat(passwordHashes.Select(h => $"{h[3]:X2}"[0]));
 
-            }
-
-            return ("Password: ", new string(password));
+            return ("Password: ", password);
         }
 
-        private IEnumerable<(int input, byte[] hash)> GetValidHashes() {
-            byte[] ToBytes(int i) => Encoding.UTF8.GetBytes($"{DoorID}{i}");
+        private void FindHashes(Func<bool> condition) {
+            _maxInput = int.MaxValue;
+            Thread[] threads = new Thread[ThreadCount];
 
-            byte[] buffer = ToBytes(0);
+            int start = (int)Math.Ceiling(_validHashes.LastOrDefault().Key / 10.0m) * 10;
 
-            for (int i = 0; i < int.MaxValue; ++i) {
-                byte[] hash = _md5.ComputeHash(buffer);
+            for (int i = 0; i < ThreadCount; i++) {
+                int threadStart = start + (i * 10);
+                threads[i] = new Thread(() => FindValidHashesThread(threadStart));
+                threads[i].Priority = ThreadPriority.Highest;
+                threads[i].Start();
+            }
 
-                if (hash[0] == 0 && hash[1] == 0 && hash[2] < 0x0F) { // Starts with 5 zeros
-                    yield return (i, hash);
+            int lastCount = _validHashes.Count;
+            while (true) {
+                if (_validHashes.Count != lastCount) {
+                    lock (_validHashes) {
+                        if (condition.Invoke()) {
+                            _maxInput = _validHashes.Last().Key;
+                            break;
+                        }
+                    }
                 }
+            }
 
-                if (buffer[buffer.Length - 1]++ == '9') {
-                    buffer = ToBytes(i);
+            while (threads.Any(t => t.IsAlive));
+        }
+
+        private void FindValidHashesThread(int start) {
+            MD5 md5 = MD5.Create();
+
+            for (int i = start; i < _maxInput; i += 10 * ThreadCount) {
+                byte[] buffer = GetInputBytes(i);
+
+                for (int j = 0; j < 10; j++, buffer[buffer.Length - 1]++) {
+                    byte[] hash = md5.ComputeHash(buffer);
+
+                    if (hash[0] + hash[1] == 0 && hash[2] < 0x10) {
+                        lock (_validHashes) {
+                            _validHashes.Add(i + j, hash);
+                        }
+                    }
                 }
             }
         }
+
+        private byte[] GetInputBytes(int input) => Encoding.ASCII.GetBytes($"{DoorID}{input}");
     }
 }
