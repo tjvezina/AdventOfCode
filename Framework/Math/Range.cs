4e14eb4b0c360ca804501cd7370251cad87cf7e6
Tyler Vezina <tvezina@karmaninteractive.com>
2020-01-23 22:35:58 -0500

    Fixed Range enumeration bug

--- Framework/Math/Range.cs
+++ Framework/Math/Range.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode {
         public int min => a < b ? a : b;
         public int max => a > b ? a : b;
         
-        public int size => max - min;
+        public int count => (max - min) + 1;
         
         public Range(int a, int b) {
             this.a = a;
@@ -26,7 +26,7 @@ namespace AdventOfCode {
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         public IEnumerator<int> GetEnumerator() {
             int sign = (a <= b ? 1 : -1);
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < count; i++) {
                 yield return a + (i * sign);
             }
         }
