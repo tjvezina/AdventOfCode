e1546a25f1198d6328e0162420fc5063fb714bb0
Tyler Vezina <tvezina@karmaninteractive.com>
2020-01-23 19:57:24 -0500

    2016 11 complete

---
+++ 2016/11/Challenge.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2016.Day11 {
+    public class Challenge : BaseChallenge {
+        public const int FloorCount = 4;
+
+        private const string PatternGenerator = @"(\w+) generator";
+        private const string PatternMicrochip = @"(\w+)-compatible microchip";
+
+        public static int ElementCount { get; private set; }
+
+        private List<int> _microchipFloors;
+        private List<int> _generatorFloors;
+
+        public override void InitPart1() {
+            MatchCollection elementMatches = Regex.Matches(inputArray.Aggregate((a, b) => a + b), PatternGenerator);
+            List<string> elements = elementMatches.Select(m => m.Groups[1].Value).ToList();
+            ElementCount = elements.Count;
+
+            _microchipFloors = new List<int>(new int[ElementCount]);
+            _generatorFloors = new List<int>(new int[ElementCount]);
+
+            for (int floor = 0; floor < FloorCount; floor++) {
+                string input = inputArray[floor];
+                
+                foreach (Match generatorMatch in Regex.Matches(input, PatternGenerator)) {
+                    string element = generatorMatch.Groups[1].Value;
+                    _generatorFloors[elements.IndexOf(element)] = floor;
+                }
+
+                foreach (Match microchipMatch in Regex.Matches(input, PatternMicrochip)) {
+                    string element = microchipMatch.Groups[1].Value;
+                    _microchipFloors[elements.IndexOf(element)] = floor;
+                }
+            }
+        }
+
+        public override string part1Answer => "31";
+        public override (string, object) SolvePart1() {
+            State initialState = new State(_microchipFloors, _generatorFloors);
+
+            return ("All objects moved to top floor in {0} moves", GetMinimumSteps(initialState));
+        }
+        
+        public override string part2Answer => "55";
+        public override (string, object) SolvePart2() {
+            // Add new equipment found on first floor
+            _microchipFloors.AddRange(new [] { 0, 0 });
+            _generatorFloors.AddRange(new [] { 0, 0 });
+
+            ElementCount = _microchipFloors.Count;
+
+            State initialState = new State(_microchipFloors, _generatorFloors);
+
+            return ("All objects moved to top floor in {0} moves", GetMinimumSteps(initialState));
+        }
+
+        private int GetMinimumSteps(State initialState) {
+            Queue<(State state, int steps)> queue = new Queue<(State, int)>(new [] { (initialState, 0) });
+            HashSet<int> visited = new HashSet<int> { initialState.GetHashCode() };
+
+            while (queue.Count > 0) {
+                (State state, int steps) = queue.Dequeue();
+
+                ElementObject[] floorObjects = state.GetElevatorFloorObjects().ToArray();
+
+                IEnumerable<State> GetNextStates(Direction direction, Range comboCountRange) {
+                    bool statesFound = false;
+                    foreach (int comboCount in comboCountRange) {
+                        if (statesFound) break;
+                        foreach (ElementObject[] objects in DataUtil.GetAllCombinations(floorObjects, comboCount)) {
+                            State nextState = state.DeepClone();
+                            if (nextState.TryStep(direction, objects) && visited.Add(nextState.GetHashCode())) {
+                                statesFound = true;
+                                yield return nextState;
+                            }
+                        }
+                    }
+                }
+
+                IEnumerable<State> nextStates = Enumerable.Empty<State>();
+                if (state.elevatorFloor < FloorCount - 1) {
+                    // If 2 objects can be moved up, don't bother moving 1
+                    nextStates = nextStates.Concat(GetNextStates(Direction.Up, new Range(2, 1)));
+                }
+                // If the floors below are empty, don't bother moving objects back down
+                if (state.elevatorFloor > 0 && state.AreObjectsBelowElevator()) {
+                    // If 1 object can be moved down, don't bother moving 2
+                    nextStates = nextStates.Concat(GetNextStates(Direction.Down, new Range(1, 2)));
+                }
+
+                foreach (State nextState in nextStates) {
+                    if (nextState.IsFinal()) {
+                        return steps + 1;
+                    }
+
+                    queue.Enqueue((nextState, steps + 1));
+                }
+            }
+
+            throw new Exception("Failed to find a sequence of steps to move all objects to top floor");
+        }
+    }
+}
---
+++ 2016/11/State.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2016.Day11 {
+    public enum Direction { Up = 1, Down = -1 }
+
+    public struct ElementObject {
+        public enum Type { Microchip, Generator }
+        
+        public int index;
+        public Type type;
+
+        public ElementObject(int index, Type type) {
+            this.index = index;
+            this.type = type;
+        }
+
+        public override string ToString() => $"{type} {index}";
+    }
+
+    public class State : IDeepCloneable<State> {
+        public int elevatorFloor { get; private set; }
+        private int[] _microchipFloors;
+        private int[] _generatorFloors;
+
+        private State() { }
+
+        public State(IList<int> microchipFloors, IList<int> generatorFloors) {
+            _microchipFloors = microchipFloors.ToArray();
+            _generatorFloors = generatorFloors.ToArray();
+            elevatorFloor = 0;
+        }
+
+        public State DeepClone() {
+            State clone = new State();
+            clone.elevatorFloor = elevatorFloor;
+            clone._microchipFloors = _microchipFloors.ToArray();
+            clone._generatorFloors = _generatorFloors.ToArray();
+            return clone;
+        }
+
+        public IEnumerable<ElementObject> GetElevatorFloorObjects() => GetFloorObjects(elevatorFloor);
+        public IEnumerable<ElementObject> GetFloorObjects(int floor) {
+            for (int i = 0; i < Challenge.ElementCount; i++) {
+                if (_microchipFloors[i] == floor) {
+                    yield return new ElementObject(i, ElementObject.Type.Microchip);
+                }
+                if (_generatorFloors[i] == floor) {
+                    yield return new ElementObject(i, ElementObject.Type.Generator);
+                }
+            }
+        }
+
+        public bool AreObjectsBelowElevator() {
+            return _microchipFloors.Any(f => f < elevatorFloor) || _generatorFloors.Any(f => f < elevatorFloor);
+        }
+
+        public bool TryStep(Direction direction, params ElementObject[] objects) {
+            int offset = (int)direction;
+            elevatorFloor += offset;
+            
+            foreach (ElementObject obj in objects) {
+                switch (obj.type) {
+                    case ElementObject.Type.Microchip:
+                        _microchipFloors[obj.index] += offset;
+                        break;
+                    case ElementObject.Type.Generator:
+                        _generatorFloors[obj.index] += offset;
+                        break;
+                }
+            }
+
+            for (int m = 0; m < Challenge.ElementCount; m++) {
+                // If a microchip is on the same floor as another element's generator without its generator, it's fried
+                int microchipFloor = _microchipFloors[m];
+                if (microchipFloor != _generatorFloors[m] && _generatorFloors.Any(g => g == microchipFloor)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsFinal() {
+            if (elevatorFloor == Challenge.FloorCount - 1) {
+                return _microchipFloors.Concat(_generatorFloors).All(f => f == Challenge.FloorCount - 1);
+            }
+            return false;
+        }
+
+        public bool Equals(State other) {
+            return elevatorFloor == other.elevatorFloor &&
+                _microchipFloors.SequenceEqual(other._microchipFloors) &&
+                _generatorFloors.SequenceEqual(other._generatorFloors);
+        }
+
+        public override bool Equals(object obj) => obj is State other && Equals(other);
+        public override int GetHashCode() {
+            // Sort element pairs, as states where pairs are swapped should be considered equivalent
+            int hashCode = Enumerable.Range(0, Challenge.ElementCount)
+                .Select(i => (_microchipFloors[i] << 2) + _generatorFloors[i])
+                .OrderBy(x => x).Aggregate((a, b) => (a << 4) | b);
+            return (hashCode << 2) + elevatorFloor;
+        }
+    }
+}
